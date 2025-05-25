96a93011e2807888fb734626332ed9259dc362b2
sheikh <sheikhzakirahmad@gmail.com>
2025-05-25 11:51:44 +0530

    feat: Update App Version, Dependencies, and Translations
    -   **App Version:**
        -   Updated `versionCode` to `60` and `versionName` to `0.7.3-dev` in `app/build.gradle.kts`.
    -   **Dependencies:**
        -   Updated `chrisbanes-haze` from `1.6.1` to `1.6.0-rc02` in `gradle/libs.versions.toml`.
    -   **Translations:**
        -   Added new Arabic (Saudi Arabia) translations in `app/src/main/res/values-ar-rSA/strings.xml`.
        -   Updated Spanish (Spain) translations in `app/src/main/res/values-es-rES/strings.xml`, including changes to `pref_app_theme_s`, `pref_enable_trash_can`, and `pref_app_lock_s` and added several new strings.
        -   Updated Ukrainian (Ukraine) translations in `app/src/main/res/values-uk-rUA/strings.xml`, including changes to `pref_app_lock_s` and added several new strings.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.googol.android.apps.photos"
         minSdk = 24
         targetSdk = 35
-        versionCode = 59
-        versionName = "0.7.2-dev"
+        versionCode = 60
+        versionName = "0.7.3-dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
